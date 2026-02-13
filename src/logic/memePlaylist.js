3ea88df35464bcd46dd20ec9b3c8523d0e22b3f6
Anket Waswani <anket.waswani@presight.ai>
2026-02-13 22:19:41 +0300

    Update meme sources to use Streamable links

--- src/logic/memePlaylist.js
+++ src/logic/memePlaylist.js
@@ -2,19 +2,19 @@ export const MEMES = [
   {
     id: "varun-smile",
     type: "video",
-    src: "https://anketmanoj.github.io/valentine/public/memes/hair.mp4",
+    src: "https://streamable.com/e/2afo4f?",
     label: "Varun dumb smile 😭",
   },
   {
     id: "maa-shakti",
     type: "video",
-    src: "https://anketmanoj.github.io/valentine/public/memes/hair.mp4",
+    src: "https://streamable.com/e/2afo4f?",
     label: "maa shakti moment ⚡",
   },
   {
     id: "arjun-stare",
     type: "video",
-    src: "https://anketmanoj.github.io/valentine/public/memes/hair.mp4",
+    src: "https://streamable.com/e/2afo4f?",
     label: "Arjun stare of judgement 🧿",
   },
 
